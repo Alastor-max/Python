05f941958b0e82920ab2812814ec6e24ad429d11
Alastor-max <moregabriel519@gmail.com>
2023-12-08 22:26:38 -0500

    Variables

---
+++ 01_Variables.py
@@ -0,0 +1,7 @@
+Variable_String = "Alastor" # las variables string sirven para texto (o eso entedi)
+print(Variable_String)
+
+Variable_int = "12" #Las Variables int sirven para numeros
+print(Variable_int)
+
+variable_booundle = 12
